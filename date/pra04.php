6ce7ee64b0ca4c84ce78b6c1e0def2c8684f252a
mokuchou <mokuchou157@gmail.com>
2022-10-18 18:51:21 +0800

    完成計算連續五個週一日期

--- date/pra04.php
+++ date/pra04.php
@@ -9,31 +9,44 @@
 </head>
 
 <body>
-    <h1>利用迴圈來計算連續五個周一的日期</h1>
-    <p>今天是<?=date("Y-m-d l");?></p>
-    <div>2022-10-24 星期一</div>
-    <div>2022-10-31 星期一</div>
-    <div>2022-11-07 星期一</div>
-    <div>2022-11-14 星期一</div>
-    <div>2022-11-21 星期一</div>
+    <h1>w-利用迴圈來計算連續五個周一的日期</h1>
+    <p>今天是<?= date("Y-m-d l"); ?></p>
     <hr>
     <?php
-    $w=date("w");
-    $diff=1-$w;
-    echo "現在距離週一".(1-$w).'天<br>';
-    for ($i=0; $i < 7; $i++) { 
-        echo "1-$i = ".(1-$i).'<br>';
+    $w = (date("w") == 0) ? 7 : date("w");
+    $diff = 1 - $w;
+    // echo "現在距離週一" . (1 - $w) . '天<br>';
+    // for ($i = 0; $i < 7; $i++) {
+    //     echo "1-$i = " . (1 - $i) . '<br>';
+    // }
+    // $firstDay = strtotime("+$diff days");
+    // echo date("Y-m-d l", $firstDay);
+    // echo '<hr>';
+
+    for ($i = 0; $i < 5; $i++) {
+        $sec = strtotime("+$diff days" . "+" . ($i + 1) . " week");
+        echo date("Y-m-d l", $sec) . '<br>';
     }
-    $firstDay=strtotime("+$diff days");
-    echo date("Y-m-d l",$firstDay);
-    echo '<hr>';
+    ?>
+    <hr>
+    <h1>N-利用迴圈來計算連續五個周一的日期</h1>
+    <p>今天是<?= date("Y-m-d l"); ?></p>
+    <hr>
+    <?php
+    $n = date("N");
+    $diff = 1 - $n;
+    echo "現在距離週一" . (1 - $n) . '天<br>';
+    // for ($i = 0; $i < 7; $i++) {
+    //     echo "1-$i = " . (1 - $i) . '<br>';
+    // }
+    // $firstDay = strtotime("+$diff days");
+    // echo date("Y-m-d l", $firstDay);
+    // echo '<hr>';
 
-    for ($i=0; $i < 5; $i++) { 
-        $sec=strtotime("+".($i+1)." week");
-        echo date("Y-m-d l",$sec).'<br>';
+    for ($i = 0; $i < 5; $i++) {
+        $sec = strtotime("+$diff days" . "+" . ($i + 1) . " week");
+        echo date("Y-m-d l", $sec) . '<br>';
     }
-
-
     ?>
 
 </body>
